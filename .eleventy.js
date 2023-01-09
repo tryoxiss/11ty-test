83f982033bd4b07fbe672ad23f7d0b30509e3133
tryoxiss <tryoxiss@gmail.com>
2023-01-08 21:11:29 -0800

    Added debug and fixed 404

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,9 @@ module.exports = function
     eleventyConfig.addPassthroughCopy("./src/res/svg/close-buttons-dark.svg");
     eleventyConfig.addPassthroughCopy("./src/res/svg/close-buttons-light.svg");
 
-    eleventyConfig.addPassthroughCopy("res");
+    eleventyConfig.addPassthroughCopy("./src/404.md");
+
+    eleventyConfig.addPassthroughCopy("res")
 
     // For images
     eleventyConfig.addPassthroughCopy({ "notes/img_post": "img_post" });
